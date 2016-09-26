90617c982d58f0d2c1347857a92ac9bae933631d
MaryamS <msarabian@repequity.com>
2016-09-26 15:53:13 -0400

    updates

--- Angular-quiz-app/Exercise-Files/Ch01/01_01/js/quiz.js
+++ Angular-quiz-app/Exercise-Files/Ch01/01_01/js/quiz.js
@@ -20,10 +20,11 @@
       $scope.selectAnswer = function(qIndex, aIndex){
 
      var questionState = $scope.myQuestions[qIndex].questionState;
-     
+
      if(questionState != 'answered'){
       $scope.myQuestions[qIndex].selectedAnswer = aIndex;
       var correctAnswer = $scope.myQuestions[qIndex].correct;
+      $scope.myQuestions[qIndex].correctAnswer = correctAnswer;
       if(aIndex === correctAnswer){
        $scope.myQuestions[qIndex].correctness ='correct';
        $scope.score += 1;
@@ -31,17 +32,26 @@
       else{
         $scope.myQuestions[qIndex].correctness = 'incorrect';
       }
-       $scope.myQuestions[qIndex].questionState ='answered';
+       $scope.myQuestions[qIndex].questionState = 'answered';
 
      }
 
       }
 
+$scope.isSelected = function(qIndex, aIndex){
+  return $scope.myQuestions[qIndex].selectedAnswer === aIndex;
+}
+$scope.isCorrect = function(qIndex, aIndex){
+  return $scope.myQuestions[qIndex].correctAnswer === aIndex;
+}
+$scope.selectContinue = function(){
 
-
+  return $scope.activeQuestion += 1;
+}
 
 
 	}]);
 
 
 })();
+
